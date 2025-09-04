1e047a9986d90758dca453904bb1105008502362
PHUCNGUYEN <phucfe050203@gmail.com>
2025-09-04 18:18:47 +0700

    update login

--- src/page/Auth/Login/Login.js
+++ src/page/Auth/Login/Login.js
@@ -39,6 +39,7 @@ function Login() {
         const token = searchParams.get('token')
         if (token) {
             localStorage.setItem('token', token)
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
             navigate('/')
         }
     }, [searchParams, navigate])
